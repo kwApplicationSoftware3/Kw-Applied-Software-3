56f06473460cd13fdd4ca25f141cc336e1cf4902
gun2935@naver.com <gun2935@naver.com>
2026-05-08 01:32:53 +0900

    feat: 모집글 수정하기

---
+++ TeamMatching.Shared/DTOs/UpdatePostRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamMatching.Shared.DTOs
+{
+    public class UpdatePostRequest
+    {
+        [Required(ErrorMessage = "제목을 입력해주세요.")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "상세 내용을 입력해주세요.")]
+        public string Content { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "요약 내용을 입력해주세요.")]
+        public string Summary { get; set; } = string.Empty;
+
+        public string? Category { get; set; } //프로젝트 분야
+        public int MaxMembers { get; set; }
+
+        public List<int> SelectedTagIds { get; set; } = new(); // 수정된 기술 스택 ID 목록
+    }
+}
