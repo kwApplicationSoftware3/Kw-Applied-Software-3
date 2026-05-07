6d1c9106465f1b3e6a7c016d49958d8535425119
gun2935@naver.com <gun2935@naver.com>
2026-05-08 01:32:17 +0900

    feat: 모집글 지원서보기

---
+++ TeamMatching.Shared/DTOs/ApplyPostResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamMatching.Shared.DTOs
+{
+    public class ApplyPostResponse
+    {
+        public bool IsSuccess { get; set; }// 성공 여부
+        public string Message { get; set; } = string.Empty; // "지원서 제출이 완료되었습니다."
+    }
+}
+
