50088bf23edfd6869f39dbf1894829c008aa29d9
gun2935@naver.com <gun2935@naver.com>
2026-05-17 17:44:41 +0900

    feat: 팀명 변경

---
+++ TeamMatching.Shared/DTOs/UpdateTeamNameRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamMatching.Shared.DTOs
+{
+    public class UpdateTeamNameRequest
+    {
+        [Required(ErrorMessage = "변경할 팀 이름을 입력해주세요.")]
+        [MaxLength(50, ErrorMessage = "팀 이름은 50자를 초과할 수 없습니다.")]
+        public string TeamName { get; set; } = string.Empty;
+    }
+}
---
+++ TeamMatching.Shared/DTOs/UpdateTeamNameResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamMatching.Shared.DTOs
+{
+    public class UpdateTeamNameResponse
+    {
+        //팀명 변경 성공 여부 플래그
+        public bool IsSuccess { get; set; }
+        //처리 결과 안내 또는 에러 메시디
+        public string Message { get; set; } = string.Empty;
+    }
+}
