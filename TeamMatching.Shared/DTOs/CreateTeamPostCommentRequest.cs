9ec07166fe8566b612289393484c535b13561f66
gun2935@naver.com <gun2935@naver.com>
2026-05-17 17:42:00 +0900

    feat: 댓글 작성

---
+++ TeamMatching.Shared/DTOs/CreateTeamPostCommentRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamMatching.Shared.DTOs
+{
+    public class CreateTeamPostCommentRequest
+    {
+        // 작성할 댓글의 본문 내용>
+        [Required(ErrorMessage = "댓글 내용을 입력해주세요.")]
+        [MaxLength(500, ErrorMessage = "댓글은 최대 500자까지 작성 가능합니다.")]
+        public string Content { get; set; } = string.Empty;
+    }
+}
---
+++ TeamMatching.Shared/DTOs/CreateTeamPostCommentResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamMatching.Shared.DTOs
+{
+    public class CreateTeamPostCommentResponse
+    {
+        // 처리 성공 여부
+        public bool IsSuccess { get; set; }
+        // 처리 결과 메시지 ("댓글 작성이 완료되었습니다.")
+        public string Message { get; set; } = string.Empty;
+    }
+}
