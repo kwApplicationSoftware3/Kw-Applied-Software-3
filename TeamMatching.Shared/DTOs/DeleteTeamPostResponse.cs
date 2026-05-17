4e4cee4f7c9d42fded845d1b9a2ae1463ae10544
gun2935@naver.com <gun2935@naver.com>
2026-05-17 17:42:42 +0900

    feat: 게시글 삭제

---
+++ TeamMatching.Shared/DTOs/DeleteTeamPostResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamMatching.Shared.DTOs
+{
+    public class DeleteTeamPostResponse
+    {
+        // 삭제 성공 여부
+        public bool IsSuccess { get; set; }
+        // 처리 결과 메시지 ("게시글이 삭제되었습니다.")
+        public string Message { get; set; } = string.Empty;
+    }
+}
