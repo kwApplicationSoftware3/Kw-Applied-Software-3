ad6770274a035be5cc5b7b019c28787c3d5968e1
gun2935@naver.com <gun2935@naver.com>
2026-05-17 17:42:24 +0900

    feat: 게시글 작성

---
+++ TeamMatching.Shared/DTOs/CreateTeamPostRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamMatching.Shared.DTOs
+{
+    public class CreateTeamPostRequest
+    {
+        //게시글 제목
+        [Required(ErrorMessage = "게시글 제목을 입력해주세요.")]
+        [MaxLength(100, ErrorMessage = "제목은 100자를 넘을 수 없습니다.")]
+        public string Title { get; set; } = string.Empty;
+
+        //게시즐 상세 본문
+        [Required(ErrorMessage = "게시글 내용을 입력해주세요.")]
+        public string Content { get; set; } = string.Empty;
+    }
+}
---
+++ TeamMatching.Shared/DTOs/CreateTeamPostResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamMatching.Shared.DTOs
+{
+    public class CreateTeamPostResponse
+    {
+        // 작성 성공 여부
+        public bool IsSuccess { get; set; }
+        // 처리 결과 메시지 ("게시글 작성이 완료되었습니다.")
+        
+        public string Message { get; set; } = string.Empty;
+    }
+}
