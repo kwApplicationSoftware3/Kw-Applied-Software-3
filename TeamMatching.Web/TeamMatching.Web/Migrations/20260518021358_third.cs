bc1e7e9cc25792c95b2f2eadb609adb755e20e19
grueq <enwoo5121@gmail.com>
2026-05-18 11:16:00 +0900

    데베 수정

---
+++ TeamMatching.Web/TeamMatching.Web/Migrations/20260518021358_third.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeamMatching.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class third : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactUrl",
+                table: "Posts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContactUrl",
+                table: "Posts",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
